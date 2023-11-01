1b161e51d1c3c54cb101f3d4f511791b8a9a12b5
Merna Tarek <merna.tarek122@gmail.com>
2023-11-01 05:02:44 +0200

    Updates

---
+++ precache-manifest.a24adb55a6f0d656524f4895bbec209b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b253e589572060f2df4574e80f5c9e85",
+    "url": "/Calculating-Bills/index.html"
+  },
+  {
+    "revision": "f7832f60cad19de9e270",
+    "url": "/Calculating-Bills/static/css/main.a743a261.chunk.css"
+  },
+  {
+    "revision": "d04a08c73863bc4c02c6",
+    "url": "/Calculating-Bills/static/js/2.8c588536.chunk.js"
+  },
+  {
+    "revision": "f7832f60cad19de9e270",
+    "url": "/Calculating-Bills/static/js/main.823a5d0f.chunk.js"
+  },
+  {
+    "revision": "68e7a4517ce62a12111d",
+    "url": "/Calculating-Bills/static/js/runtime-main.79026bce.js"
+  },
+  {
+    "revision": "c0db380d932cce921822819a06c84313",
+    "url": "/Calculating-Bills/static/media/bg.c0db380d.jpg"
+  }
+]);
